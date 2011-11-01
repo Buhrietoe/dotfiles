2d3a4bbee9e4bd1a207bbc720c8b3f6624982724
Jason Gardner <buhrietoe@gmail.com>
2011-11-01 17:00:47 -0500

    Updated record video script to use rawvideo codec, should also auto figure out size of video.

--- record-video.sh
+++ record-video.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-filename=videos/Webcam/webcam-`date +%y%m%d-%Hh%M%S`.mp4
-ffmpeg -f oss -i /dev/dsp -f video4linux2 -i /dev/video0 -vcodec libx264 -acodec libfaac -vpre superfast -isync -copyts -aq 80 -threads 0 -qmax 30 -y $filename
+
+mkdir ~/videos
+filename=~/videos/webcam-`date +%y%m%d-%Hh%M%S`.avi
+ffmpeg -f video4linux2 -i /dev/video0 -vcodec rawvideo -isync -copyts -y $filename
