f37406173f53ced6e64b4ea8ad3cd654bda9dd9d
Jason Gardner <buhrietoe@gmail.com>
2012-02-21 21:03:26 -0600

    update video conversion scripts to get rid of ffmpeg presets to be more distribution agnostic

--- convert-web.sh
+++ convert-web.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-ffmpeg -i $1 -vcodec libx264 -acodec libmp3lame -vpre baseline -isync -copyts -ab 64k -threads 0 -qmax 40 -y $2 $1-converted.mp4
+ffmpeg -i $1 -vcodec libx264 -acodec libmp3lame -isync -copyts -ab 64k -threads 0 -qmax 40 -y $2 $1-converted.mp4
--- convert-webm.sh
+++ convert-webm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-ffmpeg -i $1 -vcodec libvpx -acodec libvorbis -vpre libvpx-360p -isync -copyts -aq 80 -threads 4 -y $2 $1-converted.webm
+ffmpeg -i $1 -vcodec libvpx -acodec libvorbis -isync -copyts -aq 80 -threads 3 -y $2 $1-converted.webm
