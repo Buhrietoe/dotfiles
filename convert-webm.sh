350a310fefc011459d7a7f7cc3b5da33e8d463f2
Jason Gardner <buhrietoe@gmail.com>
2012-04-05 05:09:20 -0500

    Updated convert-webm script to use a minimum quality of 30

--- convert-webm.sh
+++ convert-webm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-ffmpeg -i $1 -vcodec libvpx -acodec libvorbis -isync -copyts -aq 80 -threads 3 -y $2 $1-converted.webm
+ffmpeg -i $1 -vcodec libvpx -acodec libvorbis -isync -copyts -aq 80 -threads 3 -qmax 30 -y $2 $1-converted.webm
