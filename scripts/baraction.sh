27766347dde1af63b072f07060a8fd37e06d7cf4
Jason Gardner <buhrietoe@gmail.com>
2013-12-26 04:00:28 -0600

    updated font in baraction.sh conky bar

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-conky | dzen2 -fg "#ffffff" -bg "#202020" -ta right -w 400 -h 14 -x 950 -y 0 -fn -*-termsyn.icons-medium-r-normal-*-12-*-*-*-*-*-*-* &
+conky | dzen2 -fg "#ffffff" -bg "#202020" -ta right -w 400 -h 14 -x 950 -y 0 -fn -*-fixed-r-normal-*-13-*-*-*-*-*-*-* &
 
 exit 0
