c9ee3e22c377c1f7a792fef1f64dc6a40f7aaf7e
Jason Gardner <buhrietoe@gmail.com>
2014-02-11 18:51:37 -0600

    updated to not use dzen

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-conky | dzen2 -fg "#ffffff" -bg "#202020" -ta right -w 400 -h 14 -x 950 -y 0 -fn -*-fixed-r-normal-*-13-*-*-*-*-*-*-* &
+#conky | dzen2 -fg "#ffffff" -bg "#202020" -ta right -w 400 -h 14 -x 950 -y 0 -fn -*-fixed-r-normal-*-13-*-*-*-*-*-*-* &
+conky
 
 exit 0
