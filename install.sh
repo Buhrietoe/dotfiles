4f236b7d9ef21a42d1bf376184d97287310b7365
Jason Gardner <buhrietoe@gmail.com>
2014-11-24 20:47:55 -0600

    added .i3 dir to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp -v .* ~/
-cp -rv scripts .config ~/
+cp -rv scripts .config .i3 ~/
 git clone git://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
