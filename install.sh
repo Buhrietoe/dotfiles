1eb3c1488a18e7271099e0486e5e11bd1382692c
Jason Gardner <buhrietoe@gmail.com>
2015-06-01 13:57:11 -0500

    Use proper home variable in install script. Use logic to clone Vundle only if it does not already exist.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-cp -v .* ~/
-cp -rv bin .config .i3 .gnupg ~/
-git clone git://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
+#!/bin/bash
+cp -v .* $HOME/
+cp -rv bin .config .i3 .gnupg $HOME/
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone git://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
