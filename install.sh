14ad653e295785e2bb1501724480959677491de8
Jason Gardner <buhrietoe@gmail.com>
2015-06-08 01:43:40 -0500

    Added line to install script to initiate vim and install all plugins.

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 cp -v .* $HOME/
 cp -rv bin .config .i3 .gnupg $HOME/
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone git://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
+vim +PluginClean! +PluginUpdate +q +q
