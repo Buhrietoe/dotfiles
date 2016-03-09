2ded810f0cc242a12c077eead6654a808fd6997c
Jason Gardner <buhrietoe@gmail.com>
2016-03-09 15:44:52 -0600

    Add .atom to installer

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -v .* $HOME/
-cp -rv bin .config .i3 .gnupg $HOME/
+cp -rv bin .config .i3 .gnupg .atom $HOME/
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone git://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
 vim +PluginClean! +PluginUpdate +q +q
