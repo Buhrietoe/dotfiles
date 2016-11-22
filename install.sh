88295fbef5425b2153bbcf482f45e5b2dfbcd868
Jason Gardner <buhrietoe@gmail.com>
2016-11-21 22:35:00 -0600

    update install script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cp -v .* $HOME/
 cp -rv bin .config .i3 .gnupg .atom $HOME/
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone git://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
-vim +PluginClean! +PluginUpdate +q +q
+vim -c PluginClean! -c PluginUpdate -c GoUpdateBinaries -c q -c q
