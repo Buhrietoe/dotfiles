3c61884260eea87f07b2cfe62fee9e3ec39c14d6
Jason Gardner <buhrietoe@gmail.com>
2017-04-27 16:18:36 -0500

    Install script refactor

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cp -v .* $HOME/
-cp -rv bin .config .i3 .gnupg .atom $HOME/
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone https://github.com/VundleVim/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
-vim -c PluginClean! -c PluginUpdate -c GoUpdateBinaries -c q -c q
+cp -v .* ${HOME}/
+cp -rv bin .config .i3 .gnupg .atom ${HOME}/
+if [ ! -d ${HOME}/.vim/bundle ]; then
+    for plugin in $(cat .vimrc | grep "^Plugin" | tr -d "'" | awk '{print $2}'); do
+        git clone https://github.com/${plugin} ${HOME}/.vim/bundle/$(basename ${plugin})
+    done
+fi
