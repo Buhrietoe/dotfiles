3d1ebe043b15fffb03656ba4ae01324463985cae
Jason Gardner <buhrietoe@gmail.com>
2017-04-27 16:07:35 -0500

    fix vundle location in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -v .* $HOME/
 cp -rv bin .config .i3 .gnupg .atom $HOME/
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone git://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then git clone https://github.com/VundleVim/Vundle.vim $HOME/.vim/bundle/Vundle.vim; fi
 vim -c PluginClean! -c PluginUpdate -c GoUpdateBinaries -c q -c q
