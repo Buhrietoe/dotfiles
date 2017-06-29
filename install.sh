8c8bd636d01d2e14419cf2b6f14463f786ad61b4
Jason Gardner <buhrietoe@gmail.com>
2017-06-29 15:29:49 -0500

    Better copy

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -v .* ${HOME}/
-cp -rv bin .config .i3 .gnupg .atom ${HOME}/
+cp -r . ${HOME}/
+rm -rf ${HOME}/install.sh ${HOME}/.git
 if [ ! -d ${HOME}/.vim/bundle ]; then
     for plugin in $(cat .vimrc | grep "^Plugin" | tr -d "'" | awk '{print $2}'); do
         git clone https://github.com/${plugin} ${HOME}/.vim/bundle/$(basename ${plugin})
