8b54a6c8e5709b03e0b11eda192039356167c5fc
Jason Gardner <buhrietoe@gmail.com>
2018-08-23 11:45:11 -0500

    not using youcompleteme anymore

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ if [ ! -d ${HOME}/.vim/bundle ]; then
     for plugin in $(cat .vimrc | grep "^Plugin" | tr -d "'" | awk '{print $2}'); do
         git clone https://github.com/${plugin} ${HOME}/.vim/bundle/$(basename ${plugin})
     done
-    (cd ${HOME}/.vim/bundle/YouCompleteMe && ./install.py --go-completer)
 fi
