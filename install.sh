1bf6133eff036072050570d4ebcd98dc61ba2c68
Jason Gardner <buhrietoe@gmail.com>
2022-07-21 11:50:49 -0500

    cleanup install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cp -r . ${HOME}/
 rm -rf ${HOME}/install.sh ${HOME}/.git
-if [ ! -d ${HOME}/.vim/bundle ]; then
-    for plugin in $(cat .vimrc | grep "^Plugin" | tr -d "'" | awk '{print $2}'); do
-        git clone https://github.com/${plugin} ${HOME}/.vim/bundle/$(basename ${plugin})
-    done
-fi
