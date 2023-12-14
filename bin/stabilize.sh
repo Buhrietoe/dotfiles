131e90de01329a6477b5c093b8d24c1b8d52a475
Jason Gardner <buhrietoe@gmail.com>
2023-12-14 08:00:17 -0600

    fix zoom on optzoom 1

--- bin/stabilize.sh
+++ bin/stabilize.sh
@@ -26,7 +26,7 @@ fi
 
 case "$OPT" in
     "0") nice ffmpeg -i "$FILENAME" -vf vidstabtransform=smoothing=50:input="$TRANSFORM":optzoom=0:crop=black,unsharp=5:5:1:3:3:1 -c:v h264 -preset veryfast -crf 23 "$@" -y "$FILENAME"-stabilized.mp4;;
-    "1") nice ffmpeg -i "$FILENAME" -vf vidstabtransform=smoothing=50:input="$TRANSFORM":optzoom=1:zoom=-20:crop=black,unsharp=5:5:1:3:3:1 -c:v h264 -preset veryfast -crf 23 "$@" -y "$FILENAME"-stabilized.mp4;;
+    "1") nice ffmpeg -i "$FILENAME" -vf vidstabtransform=smoothing=50:input="$TRANSFORM":optzoom=1:zoom=5:crop=black,unsharp=5:5:1:3:3:1 -c:v h264 -preset veryfast -crf 23 "$@" -y "$FILENAME"-stabilized.mp4;;
     "2") nice ffmpeg -i "$FILENAME" -vf vidstabtransform=smoothing=50:input="$TRANSFORM":optzoom=2:zoomspeed=0.1:crop=black,unsharp=5:5:1:3:3:1 -c:v h264 -preset veryfast -crf 23 "$@" -y "$FILENAME"-stabilized.mp4;;
     *) echo Optzoom must be 0 to 2; exit 1;;
 esac
