531e7e96a9f65abd5263e895f8393e36001208a6
Jason Gardner <buhrietoe@gmail.com>
2024-05-24 08:47:26 -0500

    switch to alacritty. Start using neovim

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+return {
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+}
