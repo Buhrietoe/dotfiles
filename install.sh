45e951d68524f6771858892f9dfbab64f76627b3
Jason Gardner <buhrietoe@gmail.com>
2024-09-19 17:22:49 -0500

    fix install

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cp -r . ${HOME}/
 if [ -d /usr/share/xsessions/ ]; then
-	sudo cp -v plasma-i3.desktop /usr/share/xsessions/
+    sudo mv -v plasma-i3.desktop /usr/share/xsessions/
 else
-	rm -v ${HOME}/plasma-i3.desktop
+    rm -v ${HOME}/plasma-i3.desktop
 fi
 rm -rf ${HOME}/install.sh ${HOME}/.git
