af0d0f69a36b907e0ee4aff1e4d7a390f3d2ad45
Jason Gardner <buhrietoe@gmail.com>
2024-09-30 13:01:39 -0500

    dont auto install plasma/i3 DE

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 cp -r . ${HOME}/
-if [ -d /usr/share/xsessions/ ]; then
-    sudo mv -v ${HOME}/plasma-i3.desktop /usr/share/xsessions/
-else
-    rm -v ${HOME}/plasma-i3.desktop
-fi
 rm -rf ${HOME}/install.sh ${HOME}/.git
