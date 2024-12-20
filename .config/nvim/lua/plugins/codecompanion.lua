3c2a3ca338691062a487aa84fbb79d59e4f62469
Jason Gardner <buhrietoe@gmail.com>
2024-12-19 22:28:27 -0600

    tweak codecompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -3,7 +3,6 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
-    "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
     "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
     { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
@@ -11,6 +10,16 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      completion = {
+        enabled_providers = { "codecompanion" },
+      },
+      providers = {
+        codecompanion = {
+          name = "CodeCompanion",
+          module = "codecompanion.providers.completion.blink",
+          enabled = true,
+        },
+      },
       strategies = {
         chat = {
           adapter = "ollama",
@@ -25,6 +34,12 @@ return {
       adapters = {
         ollama = function()
           return require("codecompanion.adapters").extend("openai_compatible", {
+            name = "llama.cpp",
+            schema = {
+              num_ctx = {
+                default = 16384,
+              },
+            },
             env = {
               url = "http://127.0.0.1:8080",
               chat_url = "/v1/chat/completions",
