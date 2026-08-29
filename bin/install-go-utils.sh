c694e86e413ed51314933cdb9a15d5c42045f6f5
Jason Gardner <buhrietoe@gmail.com>
2026-08-29 15:52:53 -0500

    remove unused

--- bin/install-go-utils.sh
+++ bin/install-go-utils.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 utils=(
-    "github.com/rakyll/govalidate@latest"
-    "github.com/rakyll/hey@latest"
     "github.com/Buhrietoe/httpdir@latest"
     "github.com/sheepla/gofind@latest"
-    "github.com/claudiodangelis/qrcp@latest"
     "github.com/charmbracelet/crush@latest"
     "github.com/jesseduffield/lazygit@latest"
     "golang.org/x/tools/gopls@latest"
     #"github.com/containers/kubernetes-mcp-server/cmd/kubernetes-mcp-server@latest"
     #"github.com/Buhrietoe/todo-mcp@latest"
     #"github.com/f/mcptools/cmd/mcptools@latest"
+    #"github.com/claudiodangelis/qrcp@latest"
+    #"github.com/rakyll/govalidate@latest"
+    #"github.com/rakyll/hey@latest"
 )
 
 for util in ${utils[@]}; do
